79c30e623ac390f55708b8ef62bb7db5c5d81bde
S8market <s8contact.us@gmail.com>
2025-03-13 16:26:40 +0530

    Update vite.config.js

--- clients8/vite.config.js
+++ clients8/vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: process.env.PORT || 4000,
-    allowedHosts: ['s8-client.onrender.com'],
+    allowedHosts: ['s8-client.onrender.com', 's8market.com', 'www.s8market.com'],
   }
 });
