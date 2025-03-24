f4b24db39f180a3572b45834606d7d2295357b69
S8market <s8contact.us@gmail.com>
2025-03-24 11:53:21 +0530

    Update userController.js

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,3 +1,13 @@
+import passport from 'passport';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+import validator from 'validator';
+import { User } from './models/User'; // Ensure this is the correct path for your User model
+import { propertyModel } from './models/Property'; // Ensure this is the correct path for your Property model
+import cloudinary from 'cloudinary';
+import sendEmail from './utils/sendEmail'; // Ensure you have the correct sendEmail utility
+import { sendVerificationCode } from './utils/sendVerificationCode'; // Ensure you have the correct sendVerificationCode utility
+
 // *******************************
 // USER REGISTRATION FUNCTIONALITY
 // *******************************
@@ -57,7 +67,6 @@ export const userRegister = async (req, res) => {
       ],
       attemptedAt: { $gte: oneHourAgo }
     });
-    console.log("Recent registration attempts:", registrationAttemptsByUser);
 
     // If more than or equal to 3 attempts have been made in the last hour, deny registration
     if (registrationAttemptsByUser.length >= 3) {
@@ -123,16 +132,6 @@ async function sendVerificationCode(
         .toString()
         .split("")
         .join(" ");
-      // Uncomment and implement your SMS sending logic below if needed:
-      // await client.calls.create({
-      //   twiml: `<Response><Say>Your verification code is ${verificationCodeWithSpace}. Your verification code is ${verificationCodeWithSpace}.</Say></Response>`,
-      //   from: process.env.TWILIO_PHONE_NUMBER,
-      //   to: phone,
-      // });
-      // return res.status(200).json({
-      //   success: true,
-      //   message: `OTP sent.`,
-      // });
       return res.status(501).json({
         success: false,
         message: "Phone verification not implemented.",
@@ -166,15 +165,6 @@ export const verifyOTP = async function (req, res) {
       });
     }
 
-    function validatePhoneNumber(phone) {
-      const phoneRegex = /^\+91\d{10}$/; ////////////////////////////////////////////
-      return phoneRegex.test(phone);
-    }
-
-    // if (!validatePhoneNumber(phone)) {
-    //   return res.status(400).json({ success: false, message: "Enter valid Phone number" });
-    // }
-
     const userAllEntries = await User.find({
       $or: [
         { email, verified: false },
@@ -185,7 +175,7 @@ export const verifyOTP = async function (req, res) {
     if (!userAllEntries || userAllEntries.length === 0) {
       return res.status(404).json({ success: false, message: "User does not exist" });
     }
-    
+
     let user;
 
     if (userAllEntries.length > 1) {
@@ -365,267 +355,13 @@ export const addToSavedProperties = async (req, res) => {
     if (!propertyExist) {
       return res.status(404).json({ success: false, message: "Property not Found" });
     }
-
-    await User.findByIdAndUpdate(
-      userId, 
-      { $addToSet: { savedProperties: propertyId } }, 
-      { new: true }
-    );
-    
-    return res.status(201).json({ success: true, message: "Property added to Favourite" });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ success: false, message: error.message });
-  }
-};
-
-// *******************************
-// GET SAVED PROPERTIES FUNCTIONALITY
-// *******************************
-export const getSavedProperties = async (req, res) => {
-  try {
-    const { userId } = req.body;
-    const user = await User.findOne({ _id: userId, savedProperties: { $exists: true, $not: { $size: 0 } } });
-
-    if (!user) {
-      return res.status(404).json({ success: false, message: "No saved Properties found" });
-    }
-
-    return res.status(200).json({ success: true, savedProperties: user.savedProperties });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ success: false, message: error.message });
-  }
-};
-
-// *******************************
-// REMOVE PROPERTY FROM SAVED PROPERTIES FUNCTIONALITY
-// *******************************
-export const removeFromSavedProperties = async (req, res) => {
-  try {
-    const { userId, propertyId } = req.body;
-    const propertyExist = await User.findOne({ _id: userId, savedProperties: propertyId });
-    if (!propertyExist) {
-      return res.status(404).json({ success: false, message: "Property not Found" });
-    }
-
-    await User.findByIdAndUpdate(userId, { $pull: { savedProperties: propertyId } });
-    return res.status(200).json({ success: true, message: "Property removed from saved properties" });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ success: false, message: error.message });
-  }
-};
-
-// *******************************
-// GET PROPERTY BY ID FUNCTIONALITY
-// *******************************
-export const getPropertyById = async (req, res) => {
-  try {
-    const { id } = req.body;
-    const property = await propertyModel.findById(id);
-    return res.status(200).json({ success: true, property });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ success: false, message: error.message });
-  }
-};
-
-// *******************************
-// GET PROFILE FUNCTIONALITY
-// *******************************
-export const getProfile = async (req, res) => {
-  try {
-    const userId = req.userId;
-    const user = await User.findById(userId).select("-_id -verificationCode -verificationCodeExpire -verified -createdAt -updatedAt -__v");
-    return res.status(200).json({ success: true, user });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ success: false, message: error.message });
-  }
-};
-
-// *******************************
-// UPDATE PROFILE FUNCTIONALITY
-// *******************************
-export const updateProfile = async (req, res) => {
-  try {
-    const userId = req.userId;
-    const { name, email, phone, address, city, state, pincode } = req.body;
+    // Assuming savedProperties is a field on the User model
     const user = await User.findById(userId);
-
-    if (!user) {
-      return res.status(404).json({ success: false, message: "User not found" });
-    }
-
-    if (!name || !email || !phone || !address || !city || !state || !pincode) {
-      return res.status(400).json({ success: false, message: "Provide all the fields" });
-    }
-
-    const userData = {
-      name,
-      email,
-      phone,
-      address: {
-        address,
-        city,
-        state,
-        pincode,
-      }
-    };
-
-    user.set(userData);
+    user.savedProperties.push(propertyId);
     await user.save();
-    return res.status(200).json({ success: true, message: "Profile updated successfully" });
+    return res.status(200).json({ success: true, message: "Property added to saved" });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ success: false, message: error.message });
-  }
-};
-
-// *******************************
-// UPDATE PROFILE IMAGE FUNCTIONALITY
-// *******************************
-export const updateProfileImage = async (req, res) => {
-  try {
-    const file = req.file;
-    const userId = req.userId;
-    if (!file || file.length === 0) {
-      return res.status(400).json({ success: false, message: "No Profile" });
-    }
-
-    const uploadToCloudinary = (fileBuffer) => {
-      return new Promise((resolve, reject) => {
-        const uploadStream = cloudinary.v2.uploader.upload_stream(
-          { folder: "profile" }, // optional: specify a folder in Cloudinary
-          (error, result) => {
-            if (result) {
-              resolve(result);
-            } else {
-              reject(error);
-            }
-          }
-        );
-        uploadStream.end(fileBuffer);
-      });
-    };
-    const uploadedFile = await uploadToCloudinary(file.buffer);
-
-    const imageData = {
-      url: uploadedFile.secure_url,
-      public_id: uploadedFile.public_id,
-      fileType: file.mimetype,
-    };
-    const user = await User.findById(userId);
-
-    if (user.profileImage && user.profileImage.public_id) {
-      await cloudinary.v2.uploader.destroy(user.profileImage.public_id);
-    }
-    user.profileImage = imageData;
-    await user.save();
-
-    return res.status(200).json({ success: true, message: "Avatar Updated" });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ success: false, message: error.message });
-  }
-};
-
-// *******************************
-// CHECK AUTH FUNCTIONALITY
-// *******************************
-export const checkAuth = async (req, res) => {
-  try {
-    const userId = req.userId;
-    const user = await User.findById(userId);
-    if (!user) {
-      return res.status(401).json({ success: false, message: "Login first" });
-    }
-    return res.status(200).json({ success: true });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ success: false, message: error.message });
-  }
-};
-
-// *******************************
-// GUEST PROPERTY SHOW FUNCTIONALITY
-// *******************************
-export const getGuestProperties = async (req, res) => {
-  try {
-    const pro = await propertyModel.find({});
-    const properties = pro.slice(0, 4);
-    return res.status(200).json({ success: true, properties });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ success: false, message: error.message });
-  }
-};
-
-// *******************************
-// SEARCH PROPERTY FUNCTIONALITY
-// *******************************
-export const searchProperty = async (req, res) => {
-  try {
-    const { searchString } = req.body;
-
-    if (!searchString || searchString.length < 3) {
-      return res.status(400).json({ success: false, message: "Search string must be at least 3 characters." });
-    }
-
-    propertyModel.find({
-      $text: { $search: searchString }
-    })
-    .then(results => {
-      return res.status(200).json({ success: true, data: results });
-    })
-    .catch(err => {
-      return res.status(404).json({ success: false, message: "No properties found matching your search." });
-    });
-    
-  } catch (error) {
-    return res.status(500).json({ success: false, message: error.message });
-  }
-};
-
-// *******************************
-// CHANGE PASSWORD FUNCTIONALITY
-// *******************************
-export const changePassword = async (req, res) => {
-  try {
-    const { oldPassword, newPassword, confirmPassword } = req.body;
-    const userId = req.userId;
-    // Validate that new password and confirmation match
-    if (newPassword !== confirmPassword) {
-      return res.status(400).json({ error: "New password and confirmation do not match." });
-    }
-
-    // Validate new password length
-    if (newPassword.length < 8 || newPassword.length > 32) {
-      return res.status(400).json({ error: "Password must be between 8 and 32 characters." });
-    }
-
-    // Retrieve user with password field (not selected by default)
-    const user = await User.findById(userId).select("+password");
-    if (!user) {
-      return res.status(404).json({ error: "User not found." });
-    }
-
-    // Check if the old password matches the current password
-    const isMatch = await bcrypt.compare(oldPassword, user.password);
-    if (!isMatch) {
-      return res.status(400).json({ error: "Old password is incorrect." });
-    }
-
-    // Hash the new password and update the user
-    const salt = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(newPassword, salt);
-    // Save the updated user document
-    await user.save();
-
-    return res.status(200).json({ message: "Password updated successfully." });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: "Internal server error." });
+    return res.status(500).json({ success: false, message: "Error adding property to saved" });
   }
 };
