d853e5537b2b9c8967f177646e9648a30a8ba9c7
Vivek Deshmukh <vivekdeshmukh1718@gmail.com>
2025-04-25 15:42:51 +0530

    class-name changed

--- clients8/src/components/auctionSystem/components/ProcessStep.jsx
+++ clients8/src/components/auctionSystem/components/ProcessStep.jsx
@@ -6,71 +6,28 @@ export default function ProcessStep({ iconSrc, title, extraClasses = "" }) {
   const isUpdateStatus = iconSrc.includes("update-status.svg");
   const isGavel = iconSrc.includes("gavel.svg");
   const isUserPlus = iconSrc.includes("user-plus.svg");
-  
+
   return (
-    <div className={`flex flex-col items-center ${extraClasses}`}>
-      {isCloudUpload ? (
-        <div className="relative mb-4">
+    <div className={`flex flex-col items-center mt-6 ${extraClasses}`}>
+      {isCloudUpload || isManageList || isUpdateStatus || isGavel || isUserPlus ? (
+        <div className="relative mb-4 mt-4"> {/* Added mt-4 here */}
           <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-[#004663] rounded-full h-[185px] w-[185px]"></div>
           <div className="relative flex justify-center items-center bg-white rounded-full h-44 w-44">
             <img
               loading="lazy"
               src={iconSrc}
               alt={title}
-              className="w-28 h-28 object-contain"
-            />
-          </div>
-        </div>
-      ) : isManageList ? (
-        <div className="relative mb-4">
-          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-[#004663] rounded-full h-[185px] w-[185px]"></div>
-          <div className="relative flex justify-center items-center bg-white rounded-full h-44 w-44">
-            <img
-              loading="lazy"
-              src={iconSrc}
-              alt={title}
-              className="w-32 h-32 object-contain"
-            />
-          </div>
-        </div>
-      ) : isUpdateStatus ? (
-        <div className="relative mb-4">
-          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-[#004663] rounded-full h-[185px] w-[185px]"></div>
-          <div className="relative flex justify-center items-center bg-white rounded-full h-44 w-44">
-            <img
-              loading="lazy"
-              src={iconSrc}
-              alt={title}
-              className="w-[160px] h-[160px] object-contain scale-75"
-            />
-          </div>
-        </div>
-      ) : isGavel ? (
-        <div className="relative mb-4">
-          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-[#004663] rounded-full h-[185px] w-[185px]"></div>
-          <div className="relative flex justify-center items-center bg-white rounded-full h-44 w-44">
-            <img
-              loading="lazy"
-              src={iconSrc}
-              alt={title}
-              className="w-[140px] h-[130px] object-contain scale-80"
-            />
-          </div>
-        </div>
-      ) : isUserPlus ? (
-        <div className="relative mb-4">
-          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-[#004663] rounded-full h-[185px] w-[185px]"></div>
-          <div className="relative flex justify-center items-center bg-white rounded-full h-44 w-44">
-            <img
-              loading="lazy"
-              src={iconSrc}
-              alt={title}
-              className="w-[160px] h-[160px] object-contain scale-75"
+              className={`object-contain ${
+                isManageList ? "w-32 h-32" :
+                isUpdateStatus || isUserPlus ? "w-[160px] h-[160px] scale-75" :
+                isGavel ? "w-[140px] h-[130px] scale-80" :
+                "w-28 h-28"
+              }`}
             />
           </div>
         </div>
       ) : (
-        <div className="flex justify-center items-center bg-blue-700 rounded-full h-44 w-44 mb-4">
+        <div className="flex justify-center items-center bg-blue-700 rounded-full h-44 w-44 mb-4 mt-4">
           <img
             loading="lazy"
             src={iconSrc}
