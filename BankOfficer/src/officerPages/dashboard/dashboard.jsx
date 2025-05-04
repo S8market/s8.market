551484be1b7ffcde6fa36d34686b3ce4a8da1f96
mayani2002 <arti.agni01@gmail.com>
2025-05-04 17:18:31 +0530

    dashboard partially responsive

--- BankOfficer/src/officerPages/dashboard/dashboard.jsx
+++ BankOfficer/src/officerPages/dashboard/dashboard.jsx
@@ -6,16 +6,16 @@ import "./dashboard.scss"
 const Dashboard = () => {
   return (
     <div className="dashboard">
-    <div className="sideContainerdash">
-      <Sidebar />
-    </div>
-    <div className="dashContainer">
-     <Header />
-     <div className="mainDashboard">
-     {/* Top header */}
-     <DashCharts/>
+      <div className="sideContainerdash">
+        <Sidebar />
       </div>
-     </div>
+      <div className="dashContainer">
+        <Header />
+        <div className="mainDashboard">
+          {/* Top header */}
+          <DashCharts />
+        </div>
+      </div>
     </div>
   )
 }
